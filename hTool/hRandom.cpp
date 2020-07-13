d648c53ea589df358e598f9c8c39430f5b77c18d
haifengMC <1745502795@qq.com>
2020-07-13 21:10:05 +0800

    更新工具类，最小可用数生成器 随机数生成器

---
+++ hTool/hRandom.cpp
@@ -0,0 +1,8 @@
+#include "global.h"
+
+#include "hTool/hRandom.h"
+
+namespace hTool
+{
+	hRandom::hRandom() : gen(rd()) {}
+}
---
+++ include/hTool.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//最小可用数生成器
+#include "hTool/hNumGen.h"
+#include "hTool/hNumGenImpl.h"
+//随机数生成器
+#include "hTool/hRandom.h"
+#include "hTool/hRandomImpl.h"
